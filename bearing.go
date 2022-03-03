0f927439f44c4c8c0b73e83a8ef16aa90b586aef
jjcinaz <jjc@leapscape.com>
2022-03-02 17:13:07 -0700

    Fixed comment/documentation on Bearing function.  Added additional test cases for Bearing function.

--- bearing.go
+++ bearing.go
@@ -6,25 +6,20 @@ import (
 )
 
 // Bearing {from, to} a point
-// flag: 0    => Initial Bearing
-// flag: 1    => Initial Bearing
-// flag: nil  => Bearing
+// flag: 0    => Return a Final bearing
+// flag: 1    => Return a Back bearing
 // Result:
 //  - Type: float64
 //  - Metric: Degress from North
 func Bearing(flag int, φ1, λ1, φ2, λ2 float64) (float64, error) {
 	φ1 = Deg2Rad(φ1)
 	φ2 = Deg2Rad(φ2)
-
 	Δλ := Deg2Rad(λ2 - λ1)
-
 	bearing := Rad2Deg(
 		math.Atan2(
 			math.Sin(
 				Δλ)*math.Cos(φ2), math.Cos(φ1)*math.Sin(φ2)-math.Sin(φ1)*math.Cos(φ2)*math.Cos(Δλ)))
-
 	var incDeg float64
-
 	switch flag {
 	case 0:
 		incDeg = 360
@@ -33,6 +28,5 @@ func Bearing(flag int, φ1, λ1, φ2, λ2 float64) (float64, error) {
 	default:
 		return -1, errors.New("flag not supported")
 	}
-
 	return math.Mod(bearing+incDeg, 360), nil
 }
--- bearing_test.go
+++ bearing_test.go
@@ -5,17 +5,40 @@ import (
 	"testing"
 )
 
-func bearing(t *testing.T, flag int, expect string, φ1, λ1, φ2, λ2 float64) {
-	bearing, _ := Bearing(flag, φ1, λ1, φ2, λ2)
-	if result := fmt.Sprintf("%.2f", bearing); expect != result {
-		t.Errorf("Bearing(%d) = expect %s; result %s", flag, expect, result)
+func TestBearing(t *testing.T) {
+	type args struct {
+		flag int
+		φ1   float64
+		λ1   float64
+		φ2   float64
+		λ2   float64
 	}
-}
-
-func TestInitial_Bearing(t *testing.T) {
-	bearing(t, 0, "48.93", 50.116667, 8.683333, 52.516667, 13.383333)
-}
-
-func TestFinal_Bearing(t *testing.T) {
-	bearing(t, 1, "52.60", 52.516667, 13.383333, 50.116667, 8.683333)
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{"Test1", args{0, 32.222417, -110.852072, 32.226925, -110.861259}, "300.12", false},
+		{"Test2", args{1, 32.222417, -110.852072, 32.226925, -110.861259}, "120.12", false},
+		{"Test3", args{0, 50.116667, 8.683333, 52.516667, 13.383333}, "48.93", false},
+		{"Test4", args{1, 52.516667, 13.383333, 50.116667, 8.683333}, "52.60", false},
+		{"Straight North", args{0, 32.222417, -110.852072, 32.226925, -110.852072}, "0.00", false},
+		{"Return South", args{1, 32.222417, -110.852072, 32.226925, -110.852072}, "180.00", false},
+		{"Straight East", args{0, 32.222417, -110.861259, 32.222417, -110.852072}, "90.00", false},
+		{"Return West", args{1, 32.222417, -110.861259, 32.222417, -110.852072}, "270.00", false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := Bearing(tt.args.flag, tt.args.φ1, tt.args.λ1, tt.args.φ2, tt.args.λ2)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Bearing() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			sgot := fmt.Sprintf("%.2f", got)
+			if sgot != tt.want {
+				t.Errorf("Bearing() got = %v, want %v", sgot, tt.want)
+			}
+		})
+	}
 }
